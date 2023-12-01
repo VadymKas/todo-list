b3ed21a575fbbdded56af9b708f0c0ff700c17dc
Vadym Kashchyn <v.kaschin@gmail.com>
2023-12-01 09:57:26 +0200

    feat: added page pagination

--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -8,37 +8,55 @@ import styles from './ToDoList.module.css';
 
 const ToDoList = () => {
   const [inputValue, setInputValue] = useState('');
+  const [currentPage, setCurrentPage] = useState(0);
+
   const dispatch = useDispatch();
   const tasks = useSelector(tasksSelector);
+
+  const tasksLimit = 10;
+  const totalPages = Math.ceil(tasks.length / tasksLimit);
+  const startIndex = currentPage * tasksLimit;
+  const endIndex = startIndex + tasksLimit;
+  const dataPerPage = tasks.slice(startIndex, endIndex);
 
   const inputValueHandler = (e) => {
     setInputValue(e.target.value);
   };
 
   const addTaskHandler = () => {
-    dispatch(addTask(inputValue));
-    setInputValue('');
+    if (inputValue) {
+      dispatch(addTask(inputValue));
+      setInputValue('');
+    }
   };
 
   return (
-    <div className={styles.list}>
-      <div className={styles.list__form}>
-        <input
-          value={inputValue}
-          onChange={inputValueHandler}
-        />
-        <button onClick={addTaskHandler}>Add</button>
-      </div>
-      <div className={styles.list__items}>
-        {tasks?.map((task) => (
-          <ToDoItem
-            key={task.id}
-            id={task.id}
-            {...task}
+    <>
+      <div className={styles.list}>
+        <div className={styles.list__form}>
+          <input
+            value={inputValue}
+            onChange={inputValueHandler}
+            placeholder='Please, write your task!'
           />
+          <button onClick={addTaskHandler}>Add</button>
+        </div>
+        <div className={styles.list__items}>
+          {dataPerPage?.map((task) => (
+            <ToDoItem
+              key={task.id}
+              id={task.id}
+              {...task}
+            />
+          ))}
+        </div>
+      </div>
+      <div className={styles.pagination}>
+        {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+          <button onClick={() => setCurrentPage(page - 1)}>{page}</button>
         ))}
       </div>
-    </div>
+    </>
   );
 };
 
